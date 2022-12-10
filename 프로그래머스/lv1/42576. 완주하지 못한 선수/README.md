a0413b693ec55ee12dd1dfe27a499a24f2666a17
Donghun Seol <ssalssi1@gmail.com>
2022-12-10 16:39:05 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.51 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=javascript) 
 
 ### 성능 요약
 
-메모리: 60.9 MB, 시간: 134.71 ms
+메모리: 74.5 MB, 시간: 75.51 ms
 
 ### 구분
 
--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,15 +1,13 @@
 function solution(participant, completion) {
-    /*
-    for(let i in participant) {
-        if(completion.includes(participant[i]) == false) return participant[i];
-        completion.splice(completion.indexOf(participant[i]), 1);
-    }
-    */
-
-    participant.sort();
-    completion.sort();
-
-    for(let i in participant) {
-        if(participant[i] !== completion[i]) return participant[i];
-    }
+    const counter = {};
+    participant.forEach(person => {
+        counter[person] = (counter[person] || 0) + 1;
+    })
+    
+    completion.forEach(person => {
+        counter[person]--;
+        if (counter[person] === 0) delete counter[person];
+    })
+    
+    return Object.keys(counter)[0];
 }
