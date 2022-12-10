73cbf75df20a0a9e547c04186ec30397916c6e99
Donghun Seol <ssalssi1@gmail.com>
2022-12-10 21:37:09 +0900

    [level 2] Title: 위장, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.js
+++ 프로그래머스/lv2/42578. 위장/위장.js
@@ -5,5 +5,4 @@ function solution(clothes) {
         counter[elem[1]] = (counter[elem[1]] || 1) + 1;
     })
     return Object.values(counter).reduce((acc, cur) => acc * cur) - 1;
-    return answer;
 }
