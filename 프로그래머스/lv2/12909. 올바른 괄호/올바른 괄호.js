65ea7e1cabf16890efc90eb6d0cc7596ab106709
Donghun Seol <ssalssi1@gmail.com>
2022-12-11 10:59:48 +0900

    [level 2] Title: 올바른 괄호, Time: 6.99 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,11 +1,10 @@
 function solution(s){
-    if (s[0] === ')') return false;
     const stack = [];
     for (let i = 0; i < s.length; i++) {
         if (s[i] === '(') {
             stack.push(s[i]);
         } else if (s[i] === ')') {
-            if (!stack.pop() === '(')  return false;
+            if (stack.length === 0 || !stack.pop() === '(')  return false;
         }
     }
     if (stack.length === 0)  return true;
