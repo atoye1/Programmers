7ec39a6c2c35ddcf299438d3d92584e97f75b831
Donghun Seol <ssalssi1@gmail.com>
2023-02-25 10:11:11 +0900

    [level 1] Title: 폰켓몬, Time: 0.22 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,6 +1,6 @@
 function solution(nums) {
-    const noDuplicatePokemon = new Set(nums);
-    const pokemonVarietyCount = noDuplicatePokemon.size;
-    const pokemonCounts = nums.length;
-    return pokemonVarietyCount > pokemonCounts/2 ? pokemonCounts/2 : pokemonVarietyCount;
+  const max = nums.length / 2;
+  const arr = [...new Set(nums)];
+
+  return arr.length > max ? max : arr.length
 }
