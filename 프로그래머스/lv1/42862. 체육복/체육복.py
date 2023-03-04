47bf0ed4d7e890485cde63a9ae1c69f010e2d39a
Donghun Seol <ssalssi1@gmail.com>
2023-03-04 16:32:56 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -0,0 +1,21 @@
+def solution(n, lost, reserve):
+    net_lost = [i for i in lost if i not in reserve]
+    net_reserve = [i for i in reserve if i not in lost]
+    
+    net_lost.sort()
+    net_reserve.sort()
+    saved = 0
+    p1, p2 = (0, 0)
+    while p1 < len(net_lost) and p2 < len(net_reserve):
+        if net_lost[p1] == net_reserve[p2] - 1 or \
+            net_lost[p1] == net_reserve[p2] + 1 or \
+        	net_lost[p1] == net_reserve[p2]:
+            p1 += 1
+            p2 += 1
+            saved += 1
+        elif net_lost[p1] > net_reserve[p2]:
+            p2 += 1
+        else:
+            p1 += 1
+        
+    return n - len(net_lost) + saved
