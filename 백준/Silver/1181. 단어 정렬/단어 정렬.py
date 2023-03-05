862109eb61f2ab4527623624eb97633d48db1940
Donghun Seol <ssalssi1@gmail.com>
2023-03-05 21:56:38 +0900

    [Silver V] Title: 단어 정렬, Time: 68 ms, Memory: 35868 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -3,6 +3,7 @@ N = int(sys.stdin.readline().rstrip())
 words = [sys.stdin.readline().rstrip() for _ in range(N)]
 
 words = list(set(words))
-words.sort(key=lambda x: (len(x), x))
+words.sort()
+words.sort(key=len)
 
 print('\n'.join(words))
