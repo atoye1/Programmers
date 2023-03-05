bf6670d3c6ca8fa6935afa964ff1653ce3e8a8f1
Donghun Seol <ssalssi1@gmail.com>
2023-03-06 00:07:52 +0900

    [Silver V] Title: 나이순 정렬, Time: 272 ms, Memory: 61636 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,9 +1,10 @@
 import sys
 N = int(sys.stdin.readline())
-members = [sys.stdin.readline().rstrip().split() + [i] for i in range(N)]
+members = [sys.stdin.readline().rstrip().split() for i in range(N)]
 
-members.sort(key=lambda x: x[2])
+# members.sort(key=lambda x: x[2])
 members.sort(key=lambda x: int(x[0]))
 
+# print(members)
 for elem in members:
     print(' '.join(elem[:2]))
