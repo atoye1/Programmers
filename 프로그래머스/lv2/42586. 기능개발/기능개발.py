1d0c10244c5f4ccfff601ea19efac58221a63a95
Donghun Seol <ssalssi1@gmail.com>
2023-03-12 07:20:47 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -6,9 +6,11 @@ def solution(progresses, speeds):
     p_q = deque(progresses)
     s_q = deque(speeds)
     counts = deque()
+    
     while p_q:
         count = math.ceil((100 - p_q.popleft()) / s_q.popleft())
         counts.append(count)
+        
     while counts:
         curr_count = counts.popleft()
         completed = 1
@@ -19,4 +21,25 @@ def solution(progresses, speeds):
             else:
                 break
         answer.append(completed)
+    return answer
+
+
+
+def solution(progresses, speeds):
+    print(progresses)
+    print(speeds)
+    answer = []
+    time = 0
+    count = 0
+    while len(progresses)> 0:
+        if (progresses[0] + time*speeds[0]) >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            count += 1
+        else:
+            if count > 0:
+                answer.append(count)
+                count = 0
+            time += 1
+    answer.append(count)
     return answer
