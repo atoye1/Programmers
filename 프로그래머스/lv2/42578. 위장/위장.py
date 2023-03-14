b107375e804c64afdd0f03a843397db59fa32336
Donghun Seol <ssalssi1@gmail.com>
2023-03-14 22:21:38 +0900

    [level 2] Title: 위장, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.py
+++ 프로그래머스/lv2/42578. 위장/위장.py
@@ -2,11 +2,10 @@ def solution(clothes):
     answer = 1
     hash_map = {}
     for cloth, category in clothes:
-        if not hash_map.get(category):
-            hash_map[category] = 1
-        else:
-            hash_map[category] += 1
+        hash_map.setdefault(category, 0)
+        hash_map[category] += 1
     for key, value in hash_map.items():
+        # 아무것도 선택하지 않는 경우를 가정해야하므로 1을 더한 값을 곱한다.
     	answer *= (value + 1)
         
     # 아무것도 입지 않는 경우는 없으므로 제외한다.
