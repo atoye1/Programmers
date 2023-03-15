2f49b31558d873dde754187fa404f992479553ff
Donghun Seol <ssalssi1@gmail.com>
2023-03-15 16:28:59 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 14.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -6,6 +6,7 @@ def solution(maps):
     #maps 2차원 배열을 대상으로 bfs를 하면서 기록하고, 마지막에 값을 돌려주면 된다.
     bfs(maps, (0, 0))
     answer = maps[len(maps) - 1][len(maps[0]) - 1]
+    # 만약 answer가 그대로 1이라면 도달하지 못한 것
     answer = -1 if answer == 1 else answer
     return answer
 
@@ -17,19 +18,25 @@ def bfs(maps, start):
     
     q = deque()
     q.append((s_row, s_col))
-    visited = [[False for _ in range(len(maps[0]))] for _ in range(len(maps))]
+    # visited를 O(N)시간에 접근하도록 작성하면 시간초과가 나온다.
+    #visited = [[False for _ in range(len(maps[0]))] for _ in range(len(maps))]
+    
     while q:
         row, col = q.popleft()
         now_count = maps[row][col]
         for i in range(4):
             n_row, n_col = row + dy[i], col + dx[i]
+            # 인덱스가 범위를 벗어난 경우 처리
             if n_row < 0 or n_row > len(maps) - 1 or n_col < 0 or n_col > len(maps[0]) - 1:
                 continue
-            if visited[n_row][n_col]:
+            # 이미 방문한 경우 처리
+            #if visited[n_row][n_col]:
+            #    continue
+            # 갈수 없는 곳인 경우 처리
+            if maps[n_row][n_col] == 0 or maps[n_row][n_col] != 1:
                 continue
-            if maps[n_row][n_col] == 0:
-                continue
+            # 지금의 블록에 인접해 있으므로 +1 해준다.
             maps[n_row][n_col] = now_count + 1
             q.append((n_row, n_col))
-            visited[n_row][n_col] = True
+            #visited[n_row][n_col] = True
     return maps
