8a8875b254fbaad6e238791abbf313f28f0b8853
Donghun Seol <ssalssi1@gmail.com>
2023-03-15 09:13:29 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.17 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
@@ -0,0 +1,15 @@
+from itertools import accumulate
+
+'''
+1. 간단히 파이썬 라이브러리를 활용하면 구할 수 있다.
+2. 하드 코딩보다 성능이 우수하다.
+'''
+
+def solution(a, b):
+    # a가 크면 둘을 스왑해주자.
+    if a > b:
+        a, b = b, a
+        
+    answer = list(accumulate(range(a, b + 1)))
+    
+    return answer[-1]
