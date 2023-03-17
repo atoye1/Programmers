a4bcc0e70d43ba574997eaa5929da7c0a5a9f51c
Donghun Seol <ssalssi1@gmail.com>
2023-03-17 16:31:45 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34268 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,11 +1,12 @@
+import sys
 from collections import deque
 # 입력 처리
 
-row, col = map(int, input().split())
-map = []
+row, col = map(int, sys.stdin.readline().rstrip().split())
+maze = []
 for i in range(row):
-    converted = [int(c) for c in input()]
-    map.append(list(converted))
+    converted = [int(c) for c in sys.stdin.readline().rstrip()]
+    maze.append(list(converted))
 
 # bfs을 활용한 최단거리 찾기 문제이므로 bfs를 활용한다.
 # 유틸리티로 활용할 dx, dy 오프셋 배열도 미리 정한다.
@@ -26,20 +27,20 @@ def bfs(graph, start):
             if n_row < 0 or n_row >= row or n_col < 0 or n_col >= col:
                 continue
             # 만약 갈 수 없는 곳이라도 제낀다.
-            elif map[n_row][n_col] == 0:
+            elif graph[n_row][n_col] == 0:
                 continue
             # 만약 1이라면 (한번도 방문한적이 없는 블럭이라면) 무조건 현재 노드의 값 + 1을 배정한다.
             # 깊이 우선탐색이므로 최초 방문한 경우가 무조건 최단거리가 된다.
             # 따라서 min()등으로 비교해줄 필요가 없다.
-            elif map[n_row][n_col] == 1:
-                map[n_row][n_col] = map[c_row][c_col] + 1
+            elif graph[n_row][n_col] == 1:
+                graph[n_row][n_col] = graph[c_row][c_col] + 1
                 q.append((n_row, n_col))
             # else:
             #     q.append((n_row, n_col))
-            #     map[n_row][n_col] = min(
-            #         map[c_row][c_col] + 1, map[n_row][n_col])
+            #     graph[n_row][n_col] = min(
+            #         graph[c_row][c_col] + 1, graph[n_row][n_col])
 
 
     # 결과는 여기에 출력된다.
-bfs(map, (0, 0))
-print(map[row-1][col-1])
+bfs(maze, (0, 0))
+print(maze[row-1][col-1])
