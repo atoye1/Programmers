f7e458f0efa0cb2b5e39ed5151fd4537266f48b6
Donghun Seol <ssalssi1@gmail.com>
2023-03-17 10:20:35 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,4 +1,5 @@
--- 코드를 입력하세요
+-- 특정 조건을 만족하는 컬럼을 찾아서 정렬하기, WHERE IN ()
 SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
 FROM ANIMAL_INS
 WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+ORDER BY ANIMAL_ID
