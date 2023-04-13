9c697a783a6fc9b31eba4ad70121d1d1c7a6db67
Donghun Seol <ssalssi1@gmail.com>
2023-04-13 16:03:45 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 152 ms, Memory: 43744 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -1,9 +1,11 @@
-N = int(input())
-coords = []
-for _ in range(N):
-    x, y = map(int, input().split())
-    coords.append([x, y])
-coords.sort(key=lambda x: [x[1], x[0]])
+import sys
 
-for coord in coords:
-    print(coord[0], coord[1], sep=' ')
+# x, y간의 가중치를 정수로 변환
+def convert(s):
+    x, y = s.split()
+    return int(y) + int(x)/1000000
+
+
+arr = sys.stdin.readlines()[1:]
+arr = sorted(arr, key=lambda x: convert(x))  # 변환한 가중치로 정렬함.
+print(''.join(arr))  # print함수를 여러번 호출하면 성능상 안좋음
