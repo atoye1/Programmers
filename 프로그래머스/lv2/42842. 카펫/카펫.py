bfc673ce6b3ee3c0fbb56346e408232ab6491214
Donghun Seol <ssalssi1@gmail.com>
2023-04-14 22:18:52 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,13 +1,18 @@
 def solution(brown, yellow):
-    total = brown + yellow
-    answer = [None, None];
-    for width in range(total - 1, 0, -1):
-        if total % width != 0:
+    answer = []
+    # 노랑 길이는 전체 길이 -2
+    # 노랑 가로도 전체 너비 -2
+    # 갈색의 전체 개수는 모서리 4개 + 노랑길이 * 2 + 노랑 높이 + 2 이다. 
+    
+    # 변수 하나를 고정시켜 놓은 상태에서 반복문을 돌린다.
+    # 노랑의 가로길이를 고정시키고 돌려보자.
+    for w in range(yellow, 0, -1):
+        # 나누어 떨어지지 않으면 패스한다.
+        if yellow % w != 0:
             continue
-        height = total / width
-        calculated_y = (width - 2) * (height - 2)
-        calculated_b = total - calculated_y
-        if yellow == calculated_y and brown == calculated_b:
-            answer[0] = width
-            answer[1] = height
-            return answer
+        h = yellow // w # 나누어 떨어졌으니 당연히 w * h == yellow는 성립한다.
+        
+        if brown == 4 + (w * 2) + (h * 2):
+            answer = [w + 2, h + 2]
+            break;
+    return answer
